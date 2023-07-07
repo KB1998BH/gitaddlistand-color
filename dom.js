efbe474f5b793ca57c52a00a5d15d365819275bd
“[krishnendu <[krishnendubhunia0098@gmail.com]>
2023-07-07 12:03:52 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,47 @@
+/// examine the document object
+
+//console.dir(document);
+// console.log(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title = 123;
+
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// //document.all[10].textContent = 'hello';
+// console.log(document.forms[0]);
+// console.log(document.links);
+
+
+//console.log(document.images);
+
+//////--- select---////////
+// GETELEMENTBYID //
+//console.log(document.getElementById('header-title'))
+
+//2nd method;
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+//console.log(headerTitle);
+//headerTitle.textContent = 'Hello';
+//headerTitle.innerText = 'Goodbye';
+// console.log(headerTitle.textContent);
+// console.log(headerTitle.innerText);
+// headerTitle.innerHTML = '<h3>Hello</h3>';
+
+//////----implement css ---////
+//headerTitle.style.borderBottom = 'solid 3px #000'
+header.style.borderBottom = 'solid 3px #000'
+
+///-- GETELEMENTBYCLASSNAME ---///
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[0]);
+//add item 
+items[1].textContent = 'Hello 2';
+items[1].style.fontWeight = 'bold';
+items[1].style.color = 'green';

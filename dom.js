5c287d4e8fe38fcb0f9bfbc0f1df9352f0072f8a
“[krishnendu <[krishnendubhunia0098@gmail.com]>
2023-07-08 17:32:39 +0530

    search and description added when search then show everything

--- dom.js
+++ dom.js
@@ -124,13 +124,84 @@
 //On clicking the delete button we should be able to remove the newly created li tag
 //function removeitem 
 //delete event 
-itemList.addEventListener('click', removeItem);
-function removeItem(e){
-    if(e.target.classList.contains('delete')){
-        if(confirm('Are you sure?')){
-            var li = e.target.parentElement;
-            itemList.removeChild(li);
+// itemList.addEventListener('click', removeItem);
+// function removeItem(e){
+//     if(e.target.classList.contains('delete')){
+//         if(confirm('Are you sure?')){
+//             var li = e.target.parentElement;
+//             itemList.removeChild(li);
       
+//         }
+//     }
+// }
+
+
+
+
+
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
+
+//form submit event 
+form.addEventListener('submit', addItem);
+
+//delete event 
+//itemList.addEventListener('click', removeItem);
+
+//Filter event 
+filter.addEventListener('keyup', filterItems);
+
+// //add item 
+function addItem(e){
+    e.preventDefault();
+    //console.log(1);
+
+    // Get the input value 
+    var newItem = document.getElementById('item').value;
+
+    //create a new li element 
+    var li = document.createElement('li');
+    //add class 
+    li.className = 'list-group-item';
+    // console.log(li);
+
+    //add text node with input value 
+    li.appendChild(document.createTextNode(newItem));
+
+    // create a delete button 
+    var deleteBtn = document.createElement('button');
+
+    //add classes to delete button 
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+    //append text node 
+    deleteBtn.appendChild(document.createTextNode('x'));
+
+    //append button to li 
+    li.appendChild(deleteBtn);
+
+    //append li to list 
+    li.appendChild(deleteBtn);
+
+    itemList.appendChild(li);
+}
+
+
+//When we type on the input box show me those items from the list which match my search string
+///--Filter Items 
+function filterItems(e){
+    //convert text to lowercase 
+    var text = e.target.value.toLowerCase();
+    //Get list by tag name 
+    var items = itemList.getElementsByTagName('li');
+    //convert to an array 
+    Array.from(items).forEach(function(item){
+        var itemName = item.firstChild.textContent;
+        if(itemName.toLowerCase().indexOf(text) != -1){
+            item.style.display = 'block';
+        }else{
+            item.style.display = 'none';
         }
-    }
+    });
+    
 }
